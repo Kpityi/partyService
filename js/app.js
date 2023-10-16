444762c55d1d08c565eab497a39e3c0507cf3b13
Bluetoon <kpityi83@gmail.com>
2023-10-16 11:46:39 +0200

    Add php to registration HTML

--- js/app.js
+++ js/app.js
@@ -223,7 +223,7 @@
             // Reset user
             user.reset().then(() => {
               // Go to login
-              $state.go("login");
+              $state.go("home");
             });
           }
         };
@@ -298,19 +298,14 @@
           password: null,
         };
 
-        // Get required input elements, accept button, and modal properties
-        let inputs = document.querySelectorAll("input[required]:not(#reg_email, #reg_password,#reg_email_confirm, #reg_password_confirm)"),
-          acceptBtn = document.getElementById("accept");
-        console.log(
-          "inputs: " + $scope.model.email + "acceptBTN: " + acceptBtn
-        );
+        
 
         // Add event listener accept button.
         $scope.accept = () => {
           // Get only necessary properties
           let data = {
-            email: $scope.model.email,
-            password: $scope.model.password,
+            email: $scope.email,
+            password: $scope.password,
           };
           // Http request
           http
@@ -328,45 +323,7 @@
               $scope.$applyAsync();
               //window.history.back();
             });
-        };
-
-        // Input changed
-        $scope.changed = () => {
-          let isDisabled = false;
-
-          inputs.forEach((element) => {
-            // Get element identifier, value, belonging to it check mark, and set variable is valid to false
-            let key = element.id,
-              value = $scope.model[key],
-              checkMark = element
-                .closest(".input-group")
-                .querySelector(".check-mark"),
-              isValid = true;
-
-            // Switch input identifier
-            switch (key) {
-              case "email":
-                isValid = util.isEmail(value);
-                break;
-              case "password":
-                isValid = util.isPassword(value);
-                break;
-            }
-
-            // Check mark
-            if (isValid) checkMark.classList.add("show");
-            else checkMark.classList.remove("show");
-
-            // Check is disabled
-            isDisabled = isDisabled || !isValid;
-          });
-
-          // Set accept button
-          acceptBtn.disabled = isDisabled;
-        };
-
-        // Input changed
-        $scope.changed();
+        };        
       },
     ])
 
@@ -438,7 +395,7 @@
         };      
 
         // Create new deffered objects
-        let countries = util.deferredObj();
+        $scope.countries = util.deferredObj();
 
         //set helper
         $scope.helper = ({
@@ -454,7 +411,7 @@
         http.request($rootScope.app.commonPath+`data/countries.json`)
         .then(response => {
           $scope.helper.countries = response;
-          countries.promise.resolve();
+          $scope.countries.promise.resolve();
           
         })
         .catch(e => {
@@ -480,6 +437,42 @@
           event.currentTarget.closest('.input-group')
                             .querySelector('input').focus();         
         }; 
+        $scope.accept = () =>{
+    
+          // Get user data
+         $scope.userData={
+          first_name: $scope.values.firstName,
+          last_name: $scope.values.lastName,
+          born: moment($scope.values.dateOfBirth).format('YYYY-MM-DD'),
+          gender: $scope.values.gender,
+          country: $scope.values.country.country.toUpperCase(),
+          country_code: $scope.values.countryCode,
+          phone: $scope.values.phone,
+          city: $scope.values.city,
+          postcode: $scope.values.postcode,
+          address: $scope.values.address,
+          email: $scope.values.email,
+          password: $scope.values.password
+          }
+          console.log($scope.userData)
+          // Http request for registration
+          http.request({
+            url   : './php/register.php',
+            method: 'POST',
+            data  : $scope.userData
+          })
+          .then(response => {
+  
+            // Check success
+            if (response.affectedRows) {
+                    console.log(response.lastInsertId);
+                    $scope.$applyAsync();
+                    alert(`Registration succesfull, new User identifier: ${response.lastInsertId}`);
+            } else  alert(`Registration unsuccesfull!  ${response}`);
+          })
+          .catch(error => {$timeout(() => alert(error), 50);});
+          
+        }
       },
     ])
 
--- php/register.php
+++ php/register.php
@@ -11,7 +11,7 @@ $db = new Database();
 
 // Set query (Check user already exist)
 $query = "SELECT `id` 
-					FROM 	`user` 
+					FROM 	`users` 
 					WHERE `email` = :email
 					LIMIT 1;";
 
@@ -28,20 +28,13 @@ if (!is_null($result)) {
 }
 
 // Set query
-$query = "INSERT INTO `user`
-					(`type`, `prefix_name`, `first_name`, `middle_name`, `last_name`, `suffix_name`, `nick_name`, `born`, `gender`, `img`, `img_type`, `country`, `country_code`, `phone`, `city`, `postcode`, `address`, `email`, `password`, `created`, `verification_code`) 
+$query = "INSERT INTO `users`
+					(`first_name`,`last_name`, `born`, `gender_id`, `country`, `country_code`, `phone`, `city`, `postcode`, `address`, `email`, `password`, `created`, `verification_code`) 
 					 VALUES 
-					('N', :prefix_name, :first_name, :middle_name, :last_name, :suffix_name, :nick_name, :born, :gender, :img, :img_type, :country, :country_code, :phone, :city, :postcode, :address, :email, :password,  :created, :verification_code)";
+					(:first_name, :last_name, :born, :gender, :country, :country_code, :phone, :city, :postcode, :address, :email, :password,  :created, :verification_code)";
 
 // Creates a new password hash
 $args['password'] = password_hash($args['password'], PASSWORD_DEFAULT);
-
-// Check image exist
-if ($args['img']) {
-
-	// Decode image
-	$args['img'] = Util::base64Decode($args['img']);
-}
 
 // Set random verification code
 $args['verification_code'] = bin2hex(random_bytes(16));
