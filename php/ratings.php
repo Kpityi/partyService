628526feca2d99505f3625ca9eebad421c20a0ec
lightszabber <hevesiszabi@gmail.com>
2023-11-06 10:37:30 +0100

    feature/design3

---
+++ php/ratings.php
@@ -0,0 +1,29 @@
+<?php
+
+// Set environment
+require_once('../../../common/php/environment.php');
+
+// Connect to database
+$db = new Database();
+
+// Set query
+$query = "SELECT `rating`, 
+                 `rating_text`, 
+                 `rating_answer`,
+                 `date`, 
+                 `users`.`last_name`, 
+                  BASE64_ENCODE(`users`.`img`) AS `img`, 
+                 `users`.`img_type`, 
+                 `users`.`gender_id`  
+          FROM `ratings`
+          INNER JOIN `users` 
+          ON `user_id` = `users`.`id`;";
+
+// Execute query with argument
+$result = $db->execute($query);
+
+// Close connection
+$db = null;        
+
+// Set response
+Util::setResponse($result);
